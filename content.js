c6715c7b821944fad62456613a5e610efbff2056
Simerca <contactsimerca@gmail.com>
2023-01-25 12:41:45 +0100

    🚀 First commit

---
+++ content.js
@@ -0,0 +1,17 @@
+//text to find
+const textToFind = "chatgpt";
+
+// Find all p, span and div elements in the document
+const elements = document.querySelectorAll("p, span, div.feed-shared-update-v2");
+
+console.log("test")
+// Iterate through all elements
+for (let i = 0; i < elements.length; i++) {
+    console.log(elements)
+  // Check if the text you want to find is in the element
+  if (elements[i].textContent.toLowerCase().indexOf(textToFind) !== -1) {
+    // Get the parent element
+    elements[i].textContent = "";
+      console.log("supprimé")
+  }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Bangpt
+
+Bangpt removes all ChatGPT mentions from the Linkedin feed
+
+## Instalation
+`git clone https://github.com/Simerca/bangpt`
+go to `chrome://extensions`
+Activate `Developper Mode`
+Click on `Load unpacked extensions`
+
+That's all
+
+## Author
+Ayrton Lecoutre
